7003d0dfc20d3f30c3940178eacd99e353493b0c
uns-miyama <miyama@uniss.co.jp>
2022-09-22 16:51:31 +0900

    changed size

--- main.tf
+++ main.tf
@@ -14,6 +14,6 @@ module "devinstance" {
 	source  = "app.terraform.io/aucnet-inc/devinstance/aws"
 	version = "0.0.2"
 
-	hello_tf_instance_type = "t2.small"
+	hello_tf_instance_type = "t2.large"
       prefix = "miyama"
 }
