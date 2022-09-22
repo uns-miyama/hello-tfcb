5914fc4dfee7a5a8261f5a3f0b8c06f56104ab34
uns-miyama <miyama@uniss.co.jp>
2022-09-22 16:00:17 +0900

    ssh enabled

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "aucnet-inc"
     workspaces {
       name = "hello-tf"
@@ -11,7 +11,7 @@ terraform {
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
-  region = var.region
+  region     = var.region
 }
 
 resource "aws_vpc" "hashicat" {
@@ -26,7 +26,57 @@ resource "aws_subnet" "hashicat" {
 
 resource "aws_instance" "hashicat" {
   ami                         = var.ami
-  count                       = var.hello_tf_instance_count
   instance_type               = var.hello_tf_instance_type
   subnet_id                   = aws_subnet.hashicat.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.hashicat.id]
+}
+
+resource "aws_security_group" "hashicat" {
+  name = "hashicat-security-group"
+
+  vpc_id = aws_vpc.hashicat.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+}
+
+resource "aws_internet_gateway" "hashicat" {
+  vpc_id = aws_vpc.hashicat.id
+}
+
+resource "aws_route_table" "hashicat" {
+  vpc_id = aws_vpc.hashicat.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.hashicat.id
+  }
+}
+
+resource "aws_route_table_association" "hashicat" {
+  subnet_id      = aws_subnet.hashicat.id
+  route_table_id = aws_route_table.hashicat.id
+}
+
+resource "aws_eip" "hashicat" {
+  instance = aws_instance.hashicat.id
+  vpc      = true
+}
+
+resource "aws_eip_association" "hashicat" {
+  instance_id   = aws_instance.hashicat.id
+  allocation_id = aws_eip.hashicat.id
 }
---
+++ output.tf
@@ -0,0 +1,3 @@
+output "ip_address" {
+  value = aws_eip.hashicat.public_ip
+}
